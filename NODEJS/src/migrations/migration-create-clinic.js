1cb60bc677c63149defdeb8a4e0c777bf03ba5f2
trannhon70 <kevintran351996@gmail.com>
2022-11-08 09:34:16 +0700

    create database

---
+++ NODEJS/src/migrations/migration-create-clinic.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Clinics', {
+    //     address: DataTypes.STRING,
+    // description: DataTypes.STRING,
+    // image: DataTypes.STRING
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      address: {
+        type: Sequelize.TEXT
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+
+      
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Clinics');
+  }
+};
---
+++ NODEJS/src/route/web.js
@@ -0,0 +1,14 @@
+import express from "express"
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRouter = (app ) =>{
+    router.get('/',homeController.getHomePage);
+
+    router.get('/about',homeController.getAboutPage);
+    return app.use("/", router);
+
+}   
+
+module.exports = initWebRouter;
